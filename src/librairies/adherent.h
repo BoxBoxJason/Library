4ad404f8f359fd2b5c1b8bc9830a51a7da05847b
BoxBoxJason <62643679+BoxBoxJason@users.noreply.github.com>
2023-12-05 17:22:17 +0100

    Fichiers classes

---
+++ src/librairies/adherent.h
@@ -0,0 +1,14 @@
+#ifndef ADHERENT_H
+#define ADHERENT_H
+
+
+class Adherent
+{
+public:
+    Adherent();
+
+private:
+
+};
+
+#endif // ADHERENT_H
---
+++ src/librairies/bd.h
@@ -0,0 +1,11 @@
+#ifndef BD_H
+#define BD_H
+#include "livre.h"
+
+class BD : public Livre
+{
+public:
+    BD();
+};
+
+#endif // BD_H
